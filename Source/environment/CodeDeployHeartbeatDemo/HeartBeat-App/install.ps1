e2783c210c8e2f3a925eefc5e0dc2d9389bb7214
Vadiraj Puranik <113619300+Vadiraj-Puranik@users.noreply.github.com>
2023-09-14 17:31:51 +0530

    Delete Source/environment directory

--- Source/environment/CodeDeployHeartbeatDemo/HeartBeat-App/install.ps1
+++
@@ -1,3 +0,0 @@
-$args = "/unattended" , "C:\HeartbeatService\HeartbeatService.exe"
-Start-Process –FilePath C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe –ArgumentList $args –NoNewWindow -Wait
-Start-Service -Name AWSHeartbeatService -ErrorAction SilentlyContinue
--- Source/environment/Updated-HeartBeat-App/HeartBeat-App/copywintail.ps1
+++
@@ -1,6 +0,0 @@
-$result = test-path -path "c:\logs\wintail.exe"
-
-if ($result -like "False")
-{
-	Copy-Item "c:\temp\wintail.exe" -Destination "c:\logs\wintail.exe"
-}
